b772b2210e6b4cc6150dc95d43419675662cd9cb
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-07-02 14:17:29 +0300

    feat: коллектор.

---
+++ stats/collector/src/main/java/ru/practicum/CollectorApplication.java
@@ -0,0 +1,12 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class CollectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorApplication.class, args);
+    }
+}
